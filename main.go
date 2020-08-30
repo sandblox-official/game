bb99dc63e31b7a10278910cdc8cf614f5c77070e
Zane Finner <zanefinnergm@gmail.com>
2020-08-30 19:57:25 -0400

    request and response relationship

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	"github.com/sandblox-official/game/server"
 	"encoding/json"
 	"log"
+
+	"github.com/sandblox-official/game/server"
 )
 
 func main() {
-	myJSONString := `{"method":"play"}`
-	var myStoredVariable server.Request
-	json.Unmarshal([]byte(myJSONString), &myStoredVariable)
-	log.Println(myStoredVariable.Method)
+	port := ":8080"
+	log.Println("Server started at", port)
+	reqString := `{"method":"play"}`
+	var req server.Request
+	err := json.Unmarshal([]byte(reqString), &req)
+	if err != nil {
+		log.Fatalln("Json Unmarshal", err)
+	}
+	resp := server.Evaluate(req)
+	log.Println(resp)
+
 }
--- server/request.go
+++ server/request.go
@@ -1,7 +1,17 @@
-package server 
+package server
 
 //Request is the json struct for incoming packets
 type Request struct {
 	Method      string
 	Description string
+}
+
+//Evaluate is what decides what to do when a request comes in
+func Evaluate(req Request) Response {
+	switch req.Method {
+	case "play":
+		resp := Response{}
+		return resp
+	}
+	return Response{Method: "err"}
 }
