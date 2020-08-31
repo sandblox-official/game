3d26189529889dd44b5765511bd32688809b6b56
Zane Finner <zanefinnergm@gmail.com>
2020-08-31 12:56:41 -0400

    convert struct into raw type

--- main.go
+++ main.go
@@ -10,13 +10,24 @@ import (
 func main() {
 	port := ":8080"
 	log.Println("Server started at", port)
+
+	//Process Request
 	reqString := `{"method":"play"}`
 	var req server.Request
 	err := json.Unmarshal([]byte(reqString), &req)
 	if err != nil {
 		log.Fatalln("Json Unmarshal", err)
 	}
-	resp := server.Evaluate(req)
-	log.Println(resp)
+
+	//Create Response
+	resp, err := server.Evaluate(req)
+	if err != nil {
+		log.Println(err)
+	}
+	respRaw, err := json.Marshal(resp)
+	if err != nil {
+		log.Println("Convert response to json:", err)
+	}
+	log.Println("Response->", string(respRaw))
 
 }
